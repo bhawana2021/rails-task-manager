ffc898c3e99bb5d2c93192544ffc5a091c009b30
bhawana srivastava <bhawana.sri2021@gmail.com>
2024-01-20 16:29:07 +0100

    o2-task manager

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "posts#index
+  get "tasks", to: "tasks#index" ,   as: 'list'
+  get "tasks/new", to: "tasks#new"
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id", to: "tasks#show", as: 'task'
+  get "tasks/:id/edit", to: "tasks#edit", as: 'edit'
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy", as: 'destroy'
 end
